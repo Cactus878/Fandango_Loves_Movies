5b9c726ff0a282c7b9d762f35ad06cb333bf0636
Yanni Karlaftis <yannikarlaftis@gmail.com>
2025-05-14 16:58:22 +1000

    Created The Project
    Includes CleaningTheData_ReasonsAndSolutions.docx

---
+++ CleanFandangoData.R
@@ -0,0 +1,7 @@
+# Created: 14/05/2025 
+# Creator: Yanni Karlaftis
+# Purpose: Clean fandango_score_comparison.csv and repopulate 
+
+library(readr)
+fandango_score_comparison <- read_csv("fandango_score_comparison.csv")
+
